acdc06b7cec36d45ad640f3c9c2b26c7d7e2f3c3
Emmanuel Zeddy <51033541+zeddyemy@users.noreply.github.com>
2024-01-18 19:24:30 +0000

    chore: Reduce JWT expiration time for Access token

--- backend/app/controllers/api/auth.py
+++ backend/app/controllers/api/auth.py
@@ -401,13 +401,12 @@ class AuthController:
             # User authentication successful
             # get user from db with the email/username.
             user = get_trendit3_user(token_data['email'])
-            access_token = create_access_token(identity=user.id, expires_delta=timedelta(minutes=43200), additional_claims={'type': 'access'})
+            access_token = create_access_token(identity=user.id, expires_delta=timedelta(minutes=1440), additional_claims={'type': 'access'})
             extra_data = {'access_token':access_token}
             
-            # Create response
-            resp = make_response(success_response('User logged in successfully', 200, extra_data))
             
-            return resp
+            
+            return success_response('User logged in successfully', 200, extra_data)
         except UnsupportedMediaType as e:
             error = True
             logging.exception(f"An UnsupportedMediaType exception occurred: {e}")
