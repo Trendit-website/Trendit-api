dbc5f3a1a2dd6bb5c5bf0e248a5f10eaae974866
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-02-15 14:38:21 +0100

    created notification model
    created notification.py in models
    
    added notifications column to Trendit3User

--- backend/app/models/notification.py
+++ backend/app/models/notification.py
@@ -4,11 +4,8 @@ from sqlalchemy.orm import backref
 from app.extensions import db
 from app.utils.helpers.basic_helpers import generate_random_string
 
-<<<<<<< Updated upstream
 from app.decorators.auth import roles_required
 
-=======
->>>>>>> Stashed changes
 
 user_notification = db.Table(
     'user_notification',
@@ -16,27 +13,16 @@ user_notification = db.Table(
     db.Column('notification_id', db.Integer, db.ForeignKey('notification.id'))
 )
 
-<<<<<<< Updated upstream
 # Notification model
-=======
->>>>>>> Stashed changes
 class Notification(db.Model):
     __tablename__ = 'notification'
 
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-<<<<<<< Updated upstream
-=======
-    # userId = db.Column(db.BigInteger, db.ForeignKey('trendit3_user.id'), nullable=False)
->>>>>>> Stashed changes
     sourceId = db.Column(db.BigInteger, nullable=False)
     sourceType = db.Column(db.String(50), nullable=False)
     type = db.Column(db.SmallInteger, nullable=False, default=0)
     read = db.Column(db.Boolean, nullable=False, default=False)
-<<<<<<< Updated upstream
     trash = db.Column(db.Boolean, nullable=False, default=False)
-=======
-    trash = db.Column(db.Boolean, nullable=False, default=True)
->>>>>>> Stashed changes
     createdAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updatedAt = db.Column(db.DateTime, nullable=True, default=None)
     content = db.Column(db.Text, nullable=True, default=None)
@@ -47,7 +33,6 @@ class Notification(db.Model):
 
     def __repr__(self):
         return f'<Notification {self.id}>'
-<<<<<<< Updated upstream
     
     @classmethod
     @roles_required('admin')
@@ -55,11 +40,6 @@ class Notification(db.Model):
         # notification = cls()
         pass
         
-=======
-
-    def insert(self):
-        db.session.add(self)
->>>>>>> Stashed changes
 
     def update(self, **kwargs):
         for key, value in kwargs.items():
