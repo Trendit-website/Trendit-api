9deb031d0f991b6e2349477c592822e10f130646
Emmanuel Zeddy <51033541+zeddyemy@users.noreply.github.com>
2024-02-22 20:05:58 +0000

    quick fix

--- backend/app/controllers/api/auth.py
+++ backend/app/controllers/api/auth.py
@@ -127,7 +127,7 @@ class AuthController:
         except Exception as e:
             db.session.rollback()
             log_exception('Exception occurred during registration', e)
-            return error_response('An error occurred while processing the request.', 500)
+            return error_response(f'An error occurred while processing the request: {str(e)}', 500)
         finally:
             db.session.close()
 
