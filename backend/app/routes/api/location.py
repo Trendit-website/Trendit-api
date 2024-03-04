105db334abadb96502ddfec7aa9e23eaecd5eb7a
Emmanuel Zeddy <51033541+zeddyemy@users.noreply.github.com>
2024-03-04 08:01:02 +0000

    change request method from GET to POST for the location endpoints

--- backend/app/routes/api/location.py
+++ backend/app/routes/api/location.py
@@ -9,10 +9,10 @@ def get_countries():
     return LocationController.get_supported_countries()
 
 
-@api.route('/states', methods=['GET'])
+@api.route('/states', methods=['POST'])
 def get_states():
     return LocationController.get_supported_country_states()
 
-@api.route('/states/lga', methods=['GET'])
+@api.route('/states/lga', methods=['POST'])
 def naija_states_lga():
     return LocationController.get_naija_state_lga()
