7888e9d595a4ebb6e80e66fceb0643fdc032960d
Emmanuel Zeddy <51033541+zeddyemy@users.noreply.github.com>
2024-03-09 10:18:13 +0000

    fix

--- backend/app/utils/helpers/payment_helpers.py
+++ backend/app/utils/helpers/payment_helpers.py
@@ -86,7 +86,7 @@ def initialize_payment(user_id, data, payment_type=None, meta_data=None):
         tx_ref=response_data['data']['reference'] # transaction reference
         
         if response_data['status']:
-            transaction = Transaction(key=tx_ref, amount=amount, transaction_type='payment', status='pending', trendit3_user=Trendit3_user)
+            transaction = Transaction(key=tx_ref, amount=amount, transaction_type='payment', description=f'{payment_type} payment', status='pending', trendit3_user=Trendit3_user)
             payment = Payment(key=tx_ref, amount=amount, payment_type=payment_type, payment_method=Config.PAYMENT_GATEWAY.lower(), status='pending', trendit3_user=Trendit3_user)
             db.session.add(transaction, payment)
             db.session.commit()
