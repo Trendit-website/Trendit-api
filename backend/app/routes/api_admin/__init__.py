a2321211922d3fbe89ba004a440a6e62da739bb3
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-03-19 16:13:18 +0100

    testing admin blueprint

--- backend/app/routes/api_admin/__init__.py
+++ backend/app/routes/api_admin/__init__.py
@@ -11,3 +11,7 @@ A Flask blueprint named 'api_admin' is created to group these routes, and it is 
 from flask import Blueprint
 
 bp = Blueprint('api_admin', __name__, url_prefix='/api/admin')
+
+@bp.route('/')
+def index():
+    return 'Admin API routes'
