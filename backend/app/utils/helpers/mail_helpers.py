3f18214affcc20062538fb3877aa05667d353daa
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-03-28 12:14:04 +0100

    Update mail_helpers.py

--- backend/app/utils/helpers/mail_helpers.py
+++ backend/app/utils/helpers/mail_helpers.py
@@ -144,5 +144,5 @@ def send_async_other_email(app, user_email, email_type, amount=None, admin_login
             console_log('EXCEPTION SENDING MAIL', f'An error occurred while sending the {email_type} email type: {str(e)}')
 
 
-def send_other_emails(user_email, email_type='membership', amount=None):
-    Thread(target=send_async_other_email, args=(current_app._get_current_object(), user_email, email_type, amount)).start()
+def send_other_emails(user_email, email_type='membership', amount=None, admin_login_code=''):
+    Thread(target=send_async_other_email, args=(current_app._get_current_object(), user_email, email_type, amount, admin_login_code)).start()
