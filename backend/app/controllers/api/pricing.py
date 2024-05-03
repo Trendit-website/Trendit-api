90d978f323a164eab8e7c784b99faae6400ce99a
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-05-03 16:15:00 +0100

    update pricing

--- backend/app/controllers/api/pricing.py
+++ backend/app/controllers/api/pricing.py
@@ -19,11 +19,25 @@ class PricingController:
     @staticmethod
     def get_all_pricing():
         try:
-            pricings = {item.item_name: item.price for item in Pricing.query.all()}
+            # pricings = {item.item_name: item.price for item in Pricing.query.all()}
+            pricings = Pricing.query.all()
+
+            prices = []
+
+            for item in pricings:
+                price = {
+                    "item_name": item.item_name,
+                    "price_earn": item.price_earn,
+                    "price_pay": item.price_pay,
+                    "created_at": item.created_at,
+                    "updated_at": item.updated_at
+                }
+
+                prices.append(price)
 
             db.session.close()
 
-            return success_response('Pricing retrieved successfully', 200, extra_data=pricings)
+            return success_response('Pricing retrieved successfully', 200, extra_data=prices)
         
         except Exception as e:
             console_log("An error occurred while retrieving pricing", e)
--- backend/app/controllers/api_admin/pricing.py
+++ backend/app/controllers/api_admin/pricing.py
@@ -20,11 +20,25 @@ class PricingController:
     @staticmethod
     def get_all_pricing():
         try:
-            pricings = {item.item_name: item.price for item in Pricing.query.all()}
+            # pricings = {item.item_name: item.price for item in Pricing.query.all()}
+            pricings = Pricing.query.all()
+
+            prices = []
+
+            for item in pricings:
+                price = {
+                    "item_name": item.item_name,
+                    "price_earn": item.price_earn,
+                    "price_pay": item.price_pay,
+                    "created_at": item.created_at,
+                    "updated_at": item.updated_at
+                }
+
+                prices.append(price)
 
             db.session.close()
 
-            return success_response('Pricing retrieved successfully', 200, extra_data=pricings)
+            return success_response('Pricing retrieved successfully', 200, extra_data=prices)
         
         except Exception as e:
             console_log("An error occurred while retrieving pricing", e)
@@ -35,12 +49,13 @@ class PricingController:
         try:
             data = request.get_json()
             item_name = data.get('item_name')
-            price = data.get('price')
+            price_pay = data.get('price_pay')
+            price_earn = data.get('price_earn')
 
-            if not item_name or not price:
-                return error_response('Item name and price are required', 400)
+            if not item_name or not price_pay or not price_earn:
+                return error_response('Item name, price_pay and price_earn are required', 400)
 
-            pricing = Pricing(item_name=item_name, price=price)
+            pricing = Pricing(item_name=item_name, price_pay=price_pay, price_earn=price_earn)
             db.session.add(pricing)
             db.session.commit()
             db.session.close()
@@ -56,18 +71,25 @@ class PricingController:
         try:
             data = request.get_json()
             item_name = data.get('item_name')
-            price = data.get('price')
+            price_pay = data.get('price_pay')
+            price_earn = data.get('price_earn')
 
-            if not item_name or not price:
+            if not item_name:
                 return error_response('Item name and price are required', 400)
 
             pricing = Pricing.query.filter_by(item_name=item_name).first()
 
             if not pricing:
                 return error_response('Pricing not found', 404)
+            
+            if price_pay:
+                pricing.price_pay = price_pay
+                db.session.commit()
 
-            pricing.price = price
-            db.session.commit()
+            if price_earn:
+                pricing.price_earn = price_earn
+                db.session.commit()                
+            
             db.session.close()
 
             return success_response('Pricing updated successfully', 200)
