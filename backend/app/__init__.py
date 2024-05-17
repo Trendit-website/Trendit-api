c1f857abfa75ffad621fbc01e7f84a1eace753f8
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-05-18 00:48:00 +0100

    chore remvove uneccessary imports

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,22 +10,21 @@ sets up CORS, configures logging, registers blueprints and defines additional ap
 @Copyright © 2024 Emmanuel Olowu
 '''
 
-from flask import Flask, request
+from flask import Flask
 from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from celery import Celery
 
-from app.models.user import Trendit3User, SocialIDs, SocialLinks
-from app.models.role import create_roles,  RoleNames
+from app.models.user import Trendit3User
+from app.models.role import create_roles
 from app.models.item import Item
 from app.models.task import Task, AdvertTask, EngagementTask
 from .models.payment import Payment, Transaction, Wallet, Withdrawal
 
 from .jobs import celery_app
 from .extensions import db, mail, limiter
-from .utils.helpers.response_helpers import error_response
 from .utils.helpers.basic_helpers import log_exception
 from .utils.helpers.user_helpers import add_user_role
 from .utils.middleware import set_access_control_allows, check_emerge, json_check, ping_url
