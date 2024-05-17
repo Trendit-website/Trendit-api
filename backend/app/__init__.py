a704f4e6b05bee040cc0449ddf662a1c9c8b900b
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-05-17 11:00:51 +0100

    chore: create user socials

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -17,7 +17,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from celery import Celery
 
-from app.models.user import Trendit3User
+from app.models.user import Trendit3User, SocialIDs, SocialLinks
 from app.models.role import create_roles
 from app.models.item import Item
 from app.models.task import Task, AdvertTask, EngagementTask
@@ -26,6 +26,7 @@ from .models.payment import Payment, Transaction, Wallet, Withdrawal
 from .jobs import celery_app
 from .extensions import db, mail, limiter
 from .utils.helpers.response_helpers import error_response
+from .utils.helpers.basic_helpers import log_exception
 from .utils.middleware import set_access_control_allows, check_emerge, json_check, ping_url
 from config import Config, configure_logging, config_by_name
 
@@ -81,8 +82,27 @@ def create_app(config_name=Config.ENV):
     app.register_blueprint(debugger_bp)
     
     
+    def createSOcials ():
+        try:
+            users = Trendit3User.query().all()
+            
+            for user in users:
+                if not user.social_ids:
+                    user_social_ids = SocialIDs(trendit3_user=user)
+                    db.session.add(user_social_ids)
+                if not user.social_links:
+                    user_social_links = SocialLinks(trendit3_user=user)
+                    db.session.add(user_social_links)
+            
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            log_exception()
+        finally:
+            db.session.close()
+    
     with app.app_context():
         create_roles()  # Create roles for trendit3
-    
+        createSOcials()
     
     return app
