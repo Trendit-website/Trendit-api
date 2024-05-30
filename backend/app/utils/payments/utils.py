8aef23d0364705e306bbb6ca62cd5c9eb7682794
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-05-30 16:15:15 +0100

    update flutterwave payload

--- backend/app/utils/payments/utils.py
+++ backend/app/utils/payments/utils.py
@@ -55,8 +55,12 @@ def construct_payload(amount: int, callback_url: str, meta: dict, user: Trendit3
                 "name": user.full_name
             },
             "customizations": {
-                "title": "Pied Piper Payments",
-                "logo": "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png"
+                "title": "Trendit Media Ltd FLW",
+                "logo": "https://res.cloudinary.com/dvgthwhdn/image/upload/v1717080512/Media/ln8asniu1jleyalg7cmw.jpg"
+            },
+            "configurations": {
+                "session_duration": 20, # Session timeout in minutes (maxValue: 1440 minutes)    
+                "max_retry_attempt": 5, # Max retry (int)
             }
         }
     
