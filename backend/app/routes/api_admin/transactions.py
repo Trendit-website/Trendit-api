f72b763c6ddb18d7a2bd56a20d24f55f35cee53b
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-06-01 15:13:51 +0100

    Update transactions.py

--- backend/app/routes/api_admin/transactions.py
+++ backend/app/routes/api_admin/transactions.py
@@ -16,25 +16,25 @@ def get_user_transactions(user_id):
     return TransactionController.get_user_transactions(user_id=user_id)
 
 
-@bp.route('/user_credit_transactions/<str:user_id>', methods=['POST'])
+@bp.route('/user_credit_transactions/<user_id>', methods=['POST'])
 @roles_required('Junior Admin')
 def get_user_credit_transactions(user_id):
     return TransactionController.get_user_transactions_by_type(type='credit', user_id=user_id)
 
 
-@bp.route('/user_debit_transactions/<str:user_id>', methods=['POST'])
+@bp.route('/user_debit_transactions/<user_id>', methods=['POST'])
 @roles_required('Junior Admin')
 def get_user_debit_transactions(user_id):
     return TransactionController.get_user_transactions_by_type(type='debit', user_id=user_id)
 
 
-@bp.route('/user_payment_transactions/<str:user_id>', methods=['POST'])
+@bp.route('/user_payment_transactions/<user_id>', methods=['POST'])
 @roles_required('Junior Admin')
 def get_user_payment_transactions(user_id):
     return TransactionController.get_user_transactions_by_type(type='payment', user_id=user_id)
 
 
-@bp.route('/user_withdrawal_transactions/<str:user_id>', methods=['POST'])
+@bp.route('/user_withdrawal_transactions/<user_id>', methods=['POST'])
 @roles_required('Junior Admin')
 def get_user_withdrawal_transactions(user_id):
     return TransactionController.get_user_transactions_by_type(type='withdrawal', user_id=user_id)
