3b39f0a9866dd4c5bb1fa1b2926dcf4627251d94
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-06-04 09:05:49 +0100

    Update pricing.py

--- backend/app/controllers/api_admin/pricing.py
+++ backend/app/controllers/api_admin/pricing.py
@@ -15,6 +15,8 @@ from ...models import Pricing, PricingCategory
 from ...utils.helpers.basic_helpers import console_log
 from ...utils.helpers.user_helpers import save_pricing_icon
 from ...utils.helpers.response_helpers import error_response, success_response
+from ...utils.helpers.basic_helpers import log_exception
+from sqlalchemy.exc import ( IntegrityError, DataError, DatabaseError, InvalidRequestError, )
 
 
 class PricingController:
@@ -76,11 +78,21 @@ class PricingController:
             db.session.commit()
             db.session.close()
 
-            return success_response('Pricing added successfully', 201)
+            extra_data={'pricing_data': pricing.to_dict()}
+            api_response = success_response('Pricing added successfully', 200, extra_data)
         
+        except (DataError, DatabaseError) as e:
+            db.session.rollback()
+            log_exception('Database error occurred during registration', e)
+            api_response = error_response('Error connecting to the database.', 500)
         except Exception as e:
-            console_log("An error occurred while adding pricing", e)
-            return error_response('An error occurred while adding pricing', 500)
+            db.session.rollback()
+            log_exception('An exception occurred updating user profile.', e)
+            api_response = error_response('An error occurred while updating user profile', 500)
+        finally:
+            db.session.close()
+
+        return api_response
         
         
     @staticmethod
