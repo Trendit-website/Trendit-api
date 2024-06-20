c109799010a1b1e15d1078e10ad29aedfad0fc80
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-06-20 10:25:56 +0100

    fix

--- backend/app/routes/api/social_profile.py
+++ backend/app/routes/api/social_profile.py
@@ -3,18 +3,18 @@ from . import api
 from app.controllers.api import SocialProfileController
 
 
-@api.route('/user/social-profiles', methods=["GET"])
+@api.route('/users/social-profiles', methods=["GET"])
 @jwt_required()
 def get_social_profiles():
     return SocialProfileController.get_social_profiles()
 
 
-@api.route('/user/social-profiles/new', methods=["POST"])
+@api.route('/users/social-profiles/new', methods=["POST"])
 @jwt_required()
 def add_social_profile():
     return SocialProfileController.add_social_profile()
 
-@api.route('/user/social-profiles/<platform>', methods=["DELETE"])
+@api.route('/users/social-profiles/<platform>', methods=["DELETE"])
 @jwt_required()
 def delete_social_profile(platform):
     return SocialProfileController.delete_social_profile(platform)
