0d44ea7e6d3e52306d93f787ae6179d0140f885d
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-06-20 11:56:10 +0100

    update flask-mail config variables for SSL or TLS

--- backend/config.py
+++ backend/config.py
@@ -67,8 +67,8 @@ class Config:
     # mail configurations
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
     MAIL_PORT = os.environ.get('MAIL_PORT') or 587
-    MAIL_USE_SSL = True
-    MAIL_USE_TLS = False
+    MAIL_USE_SSL = False
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
