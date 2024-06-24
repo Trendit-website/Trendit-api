b2bc937664d652f25a0b3986205aaacdaa6b1f51
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-06-24 17:47:30 +0100

    format message sent to admins on telegram

--- backend/app/utils/helpers/telegram_bot.py
+++ backend/app/utils/helpers/telegram_bot.py
@@ -19,13 +19,16 @@ def notify_telegram_admins_new_task(task: dict):
     platform = task.get("platform")
     fee_paid = task.get("fee_paid")
     status = task.get("status")
+    target_country = task.get("target_country")
+    target_state = task.get("target_state")
+    location = f"{target_state}, {target_country}"
     date_created = task.get("date_created")
 
-    data = (f"• Task Type: {task_type} \n • Payment Status: {payment_status} \n • Platform: {platform} \n • Amount Paid: {fee_paid} \n • Status: {status} \n • Date Created: {date_created}")
+    data = (f"• Task Type: {task_type} \n • Payment Status: {payment_status} \n • Platform: {platform} \n • Amount Paid: {fee_paid} \n • Status: {status} \n • Location: {location} \n • Date Created: {date_created}")
     
     formatted_data = data + f"\n\n Use the Buttons bellow to Approve or Reject"
     
-    message = f"\n\n{label:-^12}\n {formatted_data} \n{'//':-^12}\n\n"
+    message = f"\n\n{label:-^12}\n\n {formatted_data} \n{'//':-^12}\n\n"
     
     payload = {
         'chat_id': Config.TELEGRAM_CHAT_ID,
@@ -42,19 +45,23 @@ def notify_telegram_admins_new_task(task: dict):
     console_log("response_data", response.json())
 
 
-def notify_telegram_admins_new_profile(profile : SocialMediaProfile):
-    label = f"A New Social Media Profile was just submitted for review:"
+def notify_telegram_admins_new_profile(social_profile : SocialMediaProfile):
+    user = social_profile.trendit3_user
+    username = user.username
+    full_name = f"{user.profile.firstname} {user.profile.lastname}"
     
-    profile_id = profile.id
-    profile_link = profile.link
-    platform = profile.platform
-    status = profile.status
+    profile_id = social_profile.id
+    profile_link = social_profile.link
+    platform = social_profile.platform
+    status = social_profile.status
     
-    data = (f"• Profile ID: {profile_id} \n • Platform: {platform} \n • Profile Link: {profile_link} \n • Status: {status}")
+    label = f"{username} Just submitted a New Social Media Profile for review:"
+    
+    data = (f"• Full Name: {full_name} \n • Profile ID: {profile_id} \n • Platform: {platform} \n • Profile Link: {profile_link} \n • Status: {status}")
     
     formatted_data = data + f"\n\n Use the Buttons bellow to Approve or Reject"
     
-    message = f"\n\n{label:-^12}\n {formatted_data} \n{'//':-^12}\n\n"
+    message = f"\n\n{label:-^12}\n\n {formatted_data} \n{'//':-^12}\n\n"
     
     payload = {
         'chat_id': Config.TELEGRAM_CHAT_ID,
