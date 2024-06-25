d439e9133f033cf0124386192db79fb8f060bb6d
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-06-25 13:05:56 +0100

    update

--- app/routes/api_admin/__init__.py
+++ app/routes/api_admin/__init__.py
@@ -12,7 +12,7 @@ from flask import Blueprint
 
 bp = Blueprint('api_admin', __name__, url_prefix='/api/admin')
 
-from . import auth, task_performance, dashboard, tasks, users, transactions, pricing, social_profile
+from . import auth, task_performance, dashboard, tasks, users, transactions, pricing, social_profile, wallet
 
 @bp.route('/')
 def index():
