ed776a04ed25606659d0a35369231cba358ff721
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-07-01 01:46:49 +0100

    correction: import telegram blueprint

--- app/routes/telegram/social_profiles.py
+++ app/routes/telegram/social_profiles.py
@@ -1,9 +1,9 @@
-from . import bp
+from . import telegram_bp
 from ...controllers.telegram import SocialProfilesTelegramController
 from ...decorators import roles_required
 
 
-@bp.route('/telegram/pending-socials', methods=["GET"])
+@telegram_bp.route('/telegram/pending-socials', methods=["GET"])
 @roles_required(["Super Admin", "Admin", "Junior Admin"])
 def get_pending_social_profiles():
     return SocialProfilesTelegramController.get_pending_social_profiles()
