7b6ed9c0cdf0035bc4c3e5f614a93bdcd6f4059f
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-07-09 02:19:59 +0100

    add error handlers for api blueprint

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -13,7 +13,7 @@ from flask import Blueprint, render_template
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
-from . import (auth, payment, items, item_interactions, location, social_profile, social_platforms, task, task_performance, profile, referral, religions, stats, banks, notification, settings, transactions, social_auth, pricing, task_option)
+from . import (error_handlers, auth, payment, items, item_interactions, location, social_profile, social_platforms, task, task_performance, profile, referral, religions, stats, banks, notification, settings, transactions, social_auth, pricing, task_option)
 
 @api.route("/", methods=['GET'])
 def index():
